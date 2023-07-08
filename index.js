4d9dc891fb221978a1c2d047cf89ff0d22f668d6
DKM1234 <132291576+DKM1234@users.noreply.github.com>
2023-07-09 05:15:53 +0530

    Add Versel Command

--- index.js
+++ index.js
@@ -32,7 +32,7 @@ app.use(errorHandler)
 connectDb()
     .then(() => {
         console.log('Database Connection Succeeded.')
-        app.listen(8080,
+        app.listen(personal-api-black.vercel.app,
             () => console.log('API Started at => http://localhost:8080/api/users'))
     })
     .catch(err => console.log(err))

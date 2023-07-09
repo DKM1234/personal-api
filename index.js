afaf486072a005121ccd6ff34ef54806f5c17344
DKM1234 <132291576+DKM1234@users.noreply.github.com>
2023-07-09 06:17:01 +0530

    Update Code

--- index.js
+++ index.js
@@ -1,14 +1,45 @@
-// Import packages
-const express = require("express");
-const users = require("./routes/users");
+// ...................................................................................................................
+const express = require('express')
+const cors = require('cors');
+const bodyParser = require('body-parser')
+const connectDb = require('./db.js')
+const usersRoutes = require('./controllers/users.controller.js')
+const { errorHandler } = require('./middlewares')
 
-// Middlewares
-const app = express();
-app.use(express.json());
+const app = express()
 
-// Routes
-app.use("/users", users);
+  app.use(cors());
 
-// connection
-const port = process.env.PORT || 9001;
-app.listen(port, () => console.log(`Listening to port ${port}`));
+
+  app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', '*'); // Replace '*' with your desired origin or list of allowed origins
+    res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH'); // Add the allowed HTTP methods
+    res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization');
+    
+    // Handle preflight requests
+    if (req.method === 'OPTIONS') {
+      res.sendStatus(200);
+    } else {
+      next();
+    }
+  });
+  
+
+app.use(bodyParser.json())
+app.use('/api/users', usersRoutes)
+app.use(errorHandler)
+
+connectDb()
+    .then(() => {
+        console.log('Database Connection Succeeded.')
+        app.listen(8080,
+            () => console.log('API Started at => http://localhost:8080/api/users'))
+    })
+    .catch(err => console.log(err))
+
+// ...................................................................................................................
+
+
+
+
+
